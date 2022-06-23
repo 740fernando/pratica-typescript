c1370f3165075dfef1baf610ee5387841106669d
Fernando <fernando.luiz92@hotmail.com>
2022-06-23 20:33:31 -0300

    Incluindo callbacks em funções

---
+++ 02 - Exemplos JS vs TS/void.js
@@ -0,0 +1,4 @@
+"use strict";
+function printaValoresNumericos(numero1, numero2) {
+    console.log(numero1 + numero2);
+}
---
+++ 02 - Exemplos JS vs TS/void.ts
@@ -0,0 +1,3 @@
+function printaValoresNumericos(numero1:number, numero2: number): void{
+    console.log(numero1+numero2)
+}
