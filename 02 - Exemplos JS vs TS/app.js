b0efd5b079e04037a3185d54d1eba88841aca34c
Fernando <fernando.luiz92@hotmail.com>
2022-06-23 20:23:55 -0300

    Quando usar o tipo void em funções

--- 02 - Exemplos JS vs TS/app.js
+++ 02 - Exemplos JS vs TS/app.js
@@ -1,5 +1,4 @@
 "use strict";
-function somarValoresNumericos(numero1, numero2) {
-    return numero1 + numero2;
+function printaValoresNumericos(numero1, numero2) {
+    console.log(numero1 + numero2);
 }
-console.log(somarValoresNumericos(1, 3));
