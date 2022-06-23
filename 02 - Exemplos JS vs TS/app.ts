65d09e17241a4e5f41efa94f0639503a2a9b6dfa
Fernando <fernando.luiz92@hotmail.com>
2022-06-23 07:56:21 -0300

    Relação entre tipos de variáveis

--- 02 - Exemplos JS vs TS/app.ts
+++ 02 - Exemplos JS vs TS/app.ts
@@ -1,15 +1,21 @@
 let button = document.getElementById('button');
-let input1 = document.getElementById('input1');
-let input2 = document.getElementById('input2');
+let input1 = document.getElementById('input1') as HTMLInputElement;
+let input2 = document.getElementById('input2') as HTMLInputElement;
 
-function adicionarNumeros(numero1: number,numero2: number){
+function adicionarNumeros(numero1: number,numero2: number, devePrintar: boolean, frase: string){
+    let resultado = numero1 + numero2
+    if (devePrintar){
+        console.log(frase + resultado);
+    }
     return numero1 + numero2;
 }
 
+let devePrintar = true;
+let frase = 'O valor é : ';
 if(button) {
-    button.addEventListener('click', ()=>{
+    button.addEventListener('click', ()=> {
         if(input1 && input2){
-            adicionarNumeros(Number(input1.value),Number(input2.value))
+            console.log(adicionarNumeros(Number(input1.value),Number(input2.value), devePrintar,frase));
         }
     })
 }
