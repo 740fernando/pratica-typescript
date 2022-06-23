8c4b6633dc9260ace5a27d035fac286c67958b4f
Fernando <fernando.luiz92@hotmail.com>
2022-06-23 20:41:19 -0300

    Any está de volta e sobre unknown

--- 02 - Exemplos JS vs TS/app.ts
+++ 02 - Exemplos JS vs TS/app.ts
@@ -1,23 +1,19 @@
-function printaValoresNumericos(numero1:number, numero2: number): void{
-    console.log(numero1+numero2)
-}
+let anyEstadeVolta : any;
+anyEstadeVolta = 3;
+anyEstadeVolta = 'teste'
+anyEstadeVolta = 5;
 
-function somarValoresNumericos(numero1: number, numero2: number):number{
-    return numero1+numero2;
-}
+let stringTest: string = 'verificar'
+stringTest = anyEstadeVolta;
 
-function somarValoresNumericosETratar(numero1: number, numero2: number, callback: (numero: number)=> number):number{
-    let resultado = numero1+numero2;
-    return callback(resultado);
-}
+let unknowValor: unknown;
+unknowValor = 3;
+unknowValor = 'opa';
+unknowValor = true;
+unknowValor = 'vai sim';
 
-function aoQuadrado(numero: number): number{
-    return numero * numero;
+let stringTest2: string = 'agora vai';
+//  Propriedade do tipo unknow, é obrigatório fazer uma validação para garantir que o tipo inferido é o apropriado para ser definido.
+if(typeof unknowValor == 'string'){
+    stringTest2 = unknowValor;
 }
-
-function dividirPorEleMesmo(numero: number): number{
-    return numero/numero;
-}
-
-console.log(somarValoresNumericosETratar(1,3,aoQuadrado));
-console.log(somarValoresNumericosETratar(1,3,dividirPorEleMesmo));
