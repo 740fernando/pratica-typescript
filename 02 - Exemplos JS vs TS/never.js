684d73ca13be5a170adf7a14afbb392912dc1d40
Fernando <fernando.luiz92@hotmail.com>
2022-06-23 21:19:46 -0300

    tsconfig.json -outDir e rootDir

--- 02 - Exemplos JS vs TS/never.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-function jogaErro(erro, codigo) {
-    throw { error: erro, code: codigo };
-}
-jogaErro('deu erro', 500);
-// tipo never que nunca é finalizado
--- 02 - Exemplos JS vs TS/void.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-function printaValoresNumericos(numero1, numero2) {
-    console.log(numero1 + numero2);
-}
